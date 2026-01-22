2bb2fa7603e432939f7e87032d73e42fe2d17865
14HAK <dulonmolla88@gmail.com>
2026-01-23 01:39:38 +0600

    javascript: part 1.1

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,1 +1,80 @@
-console.log('my first code: Dulon');
+// // console.log('my first code: Dulon');
+
+// // DATA TYPES:
+// // Primitive types:
+// // string, number, boolean, undefined, null, bigint, symbol:
+// console.log('my name is dulon'); // string
+// console.log(12366); // number
+// console.log(13.25);
+// console.log(true); // boolean
+// let pass; // undefined
+// console.log(pass);
+// let exam = null; // null
+// console.log(exam);
+// let total = 12n;
+// console.log(total);
+// let level1 = Symbol('declaration'); // symbol
+// let level2 = Symbol('declaration');
+// console.log(level1 === level2);
+
+// // Reference types:
+// // array, object, function, Date, Regex:
+// let sealery = [12500, 250041, 215548]; // array
+// console.log(sealery);
+// let person = {
+// 	// object
+// 	name: 'Dulon Mahadi',
+// 	age: 30,
+// };
+// console.log(person);
+
+// function delta() {
+// 	// function
+// 	console.log(sealery, person, 'nill');
+// }
+// delta();
+// console.log(new Date()); // Date
+// console.log(RegExp(/hhh/)); // regEx
+
+// // EQUALITY CHECK:
+// // == and === are not same:
+// console.log(5 == '5'); // true
+// console.log(5 === '5'); // false
+
+// // typeof operator:
+// console.log(typeof 45);
+// console.log(typeof 'dulon mahadi');
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof { name: 'dulon' });
+// console.log(typeof null);
+// console.log(typeof function name() {});
+// console.log(typeof Symbol());
+
+// // nan: not a number:
+// console.log(isNaN('st')); // true: "st" this is string, not a number.
+// console.log(isNaN(45)); // false:
+
+// // template literals: `...`
+// const name = 'dulon mahadi';
+// console.log(`my name is: ${name}.`);
+
+// // IIFE self executed function:
+// (function () {
+// 	console.log('i am self executed function: IIFE');
+// })();
+
+// // && and || operator:
+
+// console.log(true && 30);
+// console.log(false && 31);
+
+// console.log(true || 50);
+// console.log(false || 51);
+
+// // ternary operator: ?:
+// let marrid = false;
+// let limes = marrid ? 'married man' : 'unmarried man';
+// console.log(limes);
+
+// --------------- part 2 ------------------------------
