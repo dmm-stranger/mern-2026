a56d2afe136f289097f458f2dfcf2c29b4efd53b
14HAK <dulonmolla88@gmail.com>
2026-01-24 14:08:02 +0600

    javascript: part 2

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -78,3 +78,49 @@
 // console.log(limes);
 
 // --------------- part 2 ------------------------------
+// FUNCTION:
+// function declaration:
+function sum(a, b, c) {
+	return (a + b) * c;
+}
+const sumResult = sum(2, 2, 10);
+console.log(`Result: ${sumResult}`);
+
+// function expression (anonymous / named):
+const sum2 = function (a, b, c) {
+	return a * b - c;
+};
+const result = sum2(2, 2, 10);
+console.log(`repeat of: ${result}`);
+
+// arrow function(ES6):
+const sum3 = (a, b, c) => {
+	return a * 2 + b * 4 + c * 6;
+};
+console.log(`result goes to: ${sum3(2, 3, 4)}`);
+
+// function constructor:
+const greet = new Function('name', 'return `Hello, ${name}!`;');
+
+// default parameters:
+function great(name = 'dulon mahadi', age = 18) {
+	return `i am: ${name}. Age goes to: ${age}`;
+}
+console.log(great('alice'));
+
+// rest parameter:
+function memories(a, b, ...rest) {
+	console.log(a + b);
+	console.log(`rest params: ${rest}`);
+}
+memories(1, 2, 3, 4, 5);
+
+// callback functions:
+function processData(data, callback) {
+	const result = data?.toUpperCase();
+	callback(result);
+}
+
+processData('hello', function (processResult) {
+	console.log(`result is: ${processResult}`);
+});
