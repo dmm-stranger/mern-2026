25244a888c9d25c7ee588ef932cb86c5ed97ac4a
14HAK <dulonmolla88@gmail.com>
2026-02-03 10:49:37 +0600

    javascript: practice part

--- javascript/js/dom.js
+++ javascript/js/dom.js
@@ -1,9 +1,30 @@
 // DOM: catch element by id selector::
-const headerId = document.getElementById('header-section-one');
-console.log(headerId);
-console.log(headerId?.innerHTML);
-console.log(headerId?.innerText);
-console.log(headerId?.getBoundingClientRect());
-headerId.innerHTML = 'Hello World!'; // change text
+// const headerId = document.getElementById('header-section-one');
+// console.log(headerId);
+// console.log(headerId?.innerHTML);
+// console.log(headerId?.innerText);
+// console.log(headerId?.getBoundingClientRect());
+// headerId.innerHTML = 'Hello World!'; // change text
 
 // DOM API: Application Programming Interface::
+// find element using her ID:
+const navigation = document.getElementById('navigation');
+console.log(navigation);
+
+// find element by tagName:
+const list = document.getElementsByTagName('li');
+console.log(list);
+console.log(list.length);
+console.log(list[2]);
+
+// find element by className:
+const parentList = document.getElementsByClassName('parent-list');
+console.log(parentList);
+
+// find elements by its first selector querySelector:
+const firstElement = document.querySelector('.border-e');
+console.log(firstElement);
+
+// find elements by using querySelectorAll:\
+const allElements = document.querySelectorAll('.border-e');
+console.log(allElements);
