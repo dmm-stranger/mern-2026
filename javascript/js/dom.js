7242281e2471ca1494f6a112d2b49f6b9671327d
14HAK <dulonmolla88@gmail.com>
2026-02-05 02:13:52 +0600

    javascript: practice part

--- javascript/js/dom.js
+++ javascript/js/dom.js
@@ -41,9 +41,40 @@ console.log(document.documentElement);
 console.log(document.embeds);
 
 //
-console.log(document.forms);
-console.log(document.head);
-console.log(document.links);
-console.log(document.images);
-console.log(document.scripts);
-console.log(document.title);
+// console.log(document.forms);
+// console.log(document.head);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.scripts);
+// console.log(document.title);
+
+// Changing Content in HTML:
+// const logoTitle = document.getElementById('header-section-one');
+// console.log(logoTitle);
+
+// logoTitle.innerHTML = '<h1>LOGO TITLE</h1>';
+
+// logoTitle.innerHTML = 'LOGO TITLE';
+// console.log(logoTitle);
+
+// Change Image Attribute:
+const sectionImg = document.getElementById('section-image');
+console.log(sectionImg);
+
+sectionImg.src = 'https://www.w3schools.com/js/landscape.jpg';
+
+// Change CSS:
+const navSection = document.getElementById('navigation');
+console.log(navSection);
+
+navSection.style.backgroundColor = 'lightyellow';
+
+// action in a form:
+const runFormValidation = () => {
+	let inputName = document.forms['myForm']['fname'].value;
+	console.log(inputName);
+	if (inputName === '') {
+		alert('Input Name is Missing!');
+		return false;
+	}
+};
